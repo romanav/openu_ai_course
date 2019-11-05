5250b6922ae458a7385d028884bdbca58a5ebb93
Roman Averkin <romanaverkin@gmail.com>
2019-11-05 20:05:51 +0200

    removed distance calc

--- maman_11/search.py
+++ maman_11/search.py
@@ -14,30 +14,31 @@ by Pacman agents (in searchAgents.py).
 import util
 import game
 
+
 class SearchProblem:
-  """
+    """
   This class outlines the structure of a search problem, but doesn't implement
   any of the methods (in object-oriented terminology: an abstract class).
 
   You do not need to change anything in this class, ever.
   """
 
-  def getStartState(self):
-     """
+    def getStartState(self):
+        """
      Returns the start state for the search problem
      """
-     util.raiseNotDefined()
+        util.raiseNotDefined()
 
-  def isGoalState(self, state):
-     """
+    def isGoalState(self, state):
+        """
        state: Search state
 
      Returns True if and only if the state is a valid goal state
      """
-     util.raiseNotDefined()
+        util.raiseNotDefined()
 
-  def getSuccessors(self, state):
-     """
+    def getSuccessors(self, state):
+        """
        state: Search state
 
      For a given state, this should return a list of triples,
@@ -46,27 +47,27 @@ class SearchProblem:
      required to get there, and 'stepCost' is the incremental
      cost of expanding to that successor
      """
-     util.raiseNotDefined()
+        util.raiseNotDefined()
 
-  def getCostOfActions(self, actions):
-     """
+    def getCostOfActions(self, actions):
+        """
       actions: A list of actions to take
 
      This method returns the total cost of a particular sequence of actions.  The sequence must
      be composed of legal moves
      """
-     util.raiseNotDefined()
+        util.raiseNotDefined()
 
 
 def tinyMazeSearch(problem):
-  """
+    """
   Returns a sequence of moves that solves tinyMaze.  For any other
   maze, the sequence of moves will be incorrect, so only use this for tinyMaze
   """
-  from game import Directions
-  s = Directions.SOUTH
-  w = Directions.WEST
-  return  [s,s,w,s,w,w,s,w]
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return [s, s, w, s, w, w, s, w]
 
 
 def depthFirstSearch(problem):
@@ -128,6 +129,8 @@ def remove_path_tail_in_case_of_dead_end(path):
         remove_path_tail_in_case_of_dead_end(path)
 
 
+
+
 def breadthFirstSearch(problem):
     closed_set = set()
     node_path = []
@@ -135,8 +138,7 @@ def breadthFirstSearch(problem):
     fringe = util.Queue()
     fringe.push(problem.getStartState())
 
-    path={}
-    path[problem.getStartState()] = None, 1
+    path = {problem.getStartState(): None}
 
     while True:
         if fringe.isEmpty():
@@ -145,35 +147,47 @@ def breadthFirstSearch(problem):
 
         if problem.isGoalState(node):
             node_path.append((node, 0))
+            return get_directions_list_from_bfs(node, path)
         else:
             if node not in closed_set:
                 closed_set.add(node)
                 for successors in problem.getSuccessors(node):
                     if successors[0] not in closed_set:
                         fringe.push(successors[0])
-                        path[successors[0]] = node, path[node][1]+1
+                        path[successors[0]] = node
 
+bfs_path = []
+
+def get_directions_list_from_bfs(goal, path):
+    prev, count = path[goal]
+    bfs_path.append(prev)
+    get_directions_list_from_bfs(goal, path)
+
+
+
+
+    pass
 
 
 
 def uniformCostSearch(problem):
-  "Search the node of least total cost first. "
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+    "Search the node of least total cost first. "
+    "*** YOUR CODE HERE ***"
+    util.raiseNotDefined()
 
 
 def nullHeuristic(state, problem=None):
-  """
+    """
   A heuristic function estimates the cost from the current state to the nearest
   goal in the provided SearchProblem.  This heuristic is trivial.
   """
-  return 0
+    return 0
 
 
 def aStarSearch(problem, heuristic=nullHeuristic):
-  "Search the node that has the lowest combined cost and heuristic first."
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+    "Search the node that has the lowest combined cost and heuristic first."
+    "*** YOUR CODE HERE ***"
+    util.raiseNotDefined()
 
 
 # Abbreviations
